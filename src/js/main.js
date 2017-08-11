6bd093ceca9188627ff351cacfd2d5a3d2cf126c
fa123123qq <suibee001@gmail.com>
2017-08-11 19:32:23 +0800

    项目组件结构

--- src/js/main.js
+++ src/js/main.js
@@ -10,10 +10,13 @@ Vue.use(VueResource);
 //二.导入自己的组件
 
 //2.1导入自己的组件
-import ComptApp from '../component/App.vue'
+import ComptApp from '../component/App.vue' //根组件
+
+import router from './router.js';       //路由
 
 //创建一个Vue实例,关联视图,把根组件渲染到视图中
 new Vue({
     el:'#app',
-    render:c => c(ComptApp)
+    render:c => c(ComptApp),
+    router
 })
