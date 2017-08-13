68379d6815e8491e8564688b5c2120261080d0c6
fa123123qq <suibee001@gmail.com>
2017-08-13 14:58:50 +0800

    引入MintUi和Mui样式库

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,16 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
+// MintUI是vue插件，将来需要手动use才可以使用
+import MintUI from 'mint-ui';
+import 'mint-ui/lib/style.min.css';
+// 引入mui样式
+import 'mui/dist/css/mui.css';
 
 // 1.1记得手动use启用插件
 Vue.use(VueRouter);
 Vue.use(VueResource);
+Vue.use(mintUI);
 
 //二.导入自己的组件
 
