0949036be45fc9ec0f622bd507241426e506c9a8
fa123123qq <suibee001@gmail.com>
2017-08-17 21:01:21 +0800

    添加缓存,用在index的轮播图上

--- src/component/index/index.vue
+++ src/component/index/index.vue
@@ -7,7 +7,9 @@
 
 <script>
 import Cswipe from '../common/swipe.vue';
-import Cnav from './nav.vue'
+import Cnav from './nav.vue';
+//导入缓存请求
+import Http from '../../js/common/http.js';
 export default {
     data(){
         return{
@@ -17,14 +19,18 @@ export default {
     methods:{
         getLunbos(){
             let url = 'http://139.199.192.48:8888/api/getlunbo';
-            this.$http.get(url).then(rep =>{
-                let body = rep.body;
-                if(body.status ==0){
-                    this.lunbos = body.message.map(item =>{
-                        item.src = item.img;
-                        return item;
-                    })
-                }
+            // this.$http.get(url).then(rep =>{
+            //     let body = rep.body;
+            //     if(body.status ==0){
+            //         this.lunbos = body.message.map(item =>{
+            //             item.src = item.img;
+            //             return item;
+            //         })
+            //     }
+            // })
+            Http.get(url,body =>{
+                //因为公共的轮播图使用的图片地址属性名为src，这里的图片地址名为img，所以转换一下
+                this.lunbos = body.message.map(item => (item.src = item.img)&&item)
             })
         }
     },
---
+++ src/js/common/cache.js
@@ -0,0 +1,25 @@
+const cache = {};
+const maxAge = 10000;       // 10秒有效期,10秒过后要请求数据
+// 存储，存储的时候添加一个时间戳
+export default{
+    set(key,data){
+        cache[key] = {
+            data : data,
+            time : Date.now()
+        }
+    },
+// 读取，读取的时候要判断有没有过期，没有缓存数据，或者过期了就删掉数据，返回null
+    get(key){
+        let tempObj = cache[key];
+        if(!tempObj || Date.now() - tempObj.time > maxAge){
+            this.remove(key);
+        }else{
+            return tempObj.data;
+        }
+    },
+
+    //删除数据
+    remove(key){
+        delete cache[key];
+    }
+}
