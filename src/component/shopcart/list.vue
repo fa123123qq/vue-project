92dbd20966d82a4bc868b16d43d6d7e1846d6b59
fa123123qq <suibee001@gmail.com>
2017-08-19 19:55:15 +0800

    购物车数据回显和总数计算使用vuex

--- src/component/shopcart/list.vue
+++ src/component/shopcart/list.vue
@@ -4,8 +4,8 @@
     <!-- 商品列表 -->
     <div class="goods" v-for="item in shopcartList" :key="item.id">
       <mt-switch class="switch" v-model="item.selected">
-        </mt-switch>
-         <img class="img" :src="item.src">
+      </mt-switch>
+      <img class="img" :src="item.src">
       <div class="inforight">
         <h4 class="mui-ellipsis-2">{{ item.title }}</h4>
         <div class="bottom">
@@ -42,162 +42,165 @@ import config from '../../js/config.js';
 import Ctitle from '../common/title.vue';
 import goodsStorage from '../../js/model/goods.js';
 import Cnumbox from '../common/numbox.vue';
-import {Toast} from 'mint-ui';
+import { Toast } from 'mint-ui';
 export default {
-    data(){
-        return{
-            title:'购物车',
-            shopcartList:[]
+  data() {
+    return {
+      title: '购物车',
+      shopcartList: []
 
-        };
-    },
-    methods:{
-      //获取购物车商品数据
-        getShopcartList(){
-          //如果之前没有选购过商品,就不用请求接口了
-          let idList = goodsStorage.getIDList();
-          if(!idList.length){return;}
-          let url = config.shopcartList + goodsStorage.getIDList();
-          this.$http.get(url).then(rep => {
-            let body = rep.body;
-            // 因为请求回来的图片没有域名，所以加一下
+    };
+  },
+  methods: {
+    //获取购物车商品数据
+    getShopcartList() {
+      //如果之前没有选购过商品,就不用请求接口了
+      // let idList = goodsStorage.getIDList();
+      let idList = this.$store.getters.getIDList;
+      if (!idList.length) { return; }
+      let url = config.shopcartList + goodsStorage.getIDList();
+      this.$http.get(url).then(rep => {
+        let body = rep.body;
+        // 因为请求回来的图片没有域名，所以加一下
         // 每个商品都额外添加一个selected属性用来控制该商品的选取，默认选取状态
-            if(body.status == 0){
-              this.shopcartList = body.message.map(item => {
-                item.selected = true;
-                item.src = config.imgDomain + item.thumb_path;
-                return item;
-              })
-            }
-          })
-        },
-        //删除商品
-        remove(id){
-          //删除页面的商品信息
-          let index  = this.shopcartList.findIndex(item => item.id == id);    //找到要删除的id和数据的id一样
-          index >-1 && this.shopcartList.splice(index,1);
-          //删除数据的信息
-          // goodsStorage.remove(id);
-          this.$store.commit('remove',id);
-        },
-       //通过id拿到商品的选购数量
-       getTotal(id){
-          // return goodsStorage.get(id);
-          return this.$store.state[id];
-       },
-       //更新指定商品的购买数量
-       upTotal(id,total){
-         //要手动更新才能触发购买数量和总价格的修改
-         this.shopcartList[0].selected = !this.shopcartList[0].selected;
-         this.shopcartList[0].selected = !this.shopcartList[0].selected;
-        //  goodsStorage.set(id,total);
-        //   // 把商品的总数挂载上去
-        //  document.querySelector('.mui-badge').innerHTML = goodsStorage.get();
-        this.$store.commit('set',{id,total})
-     },
-       //付款
-       payment(){
-         //只要有一个商品被选中,并且不为0,那么结果就是true
-         let hasSelected = this.shopcartList.some(item => item.selected&&goodsStorage.get(item.id));
-         if(!hasSelected){
-           Toast('请至少选购一件商品');
-         }
-         //使用正则判断用户是否登陆,登陆成功后跳转到订单页
-         else if(!/SESSIONID=\w+/.test(document.cookie)){
-            // Toast('跳转到登陆');
-            this.$router.push({name:'login',query:{nextpage:'/shopcart/order'}})
-         }
-         // 选购了商品，也登陆了，才能转到订单页
-         else{
-          //  Toast('跳转到订单页面');
-          this.$router.push('/shopcart/order');
-         }
-       }
-
-    },computed:{
-       //总数量
-       sumTotal(){
-         // 遍历取出每个商品的数量，如果这个商品是未选中状态，那么数量设为0
-         let totalArr = this.shopcartList.map(item => item.selected?+goodsStorage.get(item.id):0);
-         return totalArr.length && totalArr.reduce((v1,v2)=>v1 + v2);
-       },
-        //总价格
-       priceTotal(){
-         // 遍历取出每个商品的数量，如果这个商品是未选中状态，那么数量设为0
-         let priceArr = this.shopcartList.map(item => item.selected?+goodsStorage.get(item.id)*item.sell_price:0);
-         return priceArr.length && priceArr.reduce((v1,v2)=>v1 + v2);
-       }
+        if (body.status == 0) {
+          this.shopcartList = body.message.map(item => {
+            item.selected = true;
+                  item.src = config.imgDomain + item.thumb_path;
+                  return item;
+                })
+              }
+      })
     },
-    created(){
-        this.getShopcartList();
+    //删除商品
+    remove(id) {
+      //删除页面的商品信息
+      let index = this.shopcartList.findIndex(item => item.id == id);    //找到要删除的id和数据的id一样
+      index > -1 && this.shopcartList.splice(index, 1);
+      //删除数据的信息
+      // goodsStorage.remove(id);
+      this.$store.commit('remove', id);
     },
-    components:{
-      "v-title":Ctitle,
-       'v-numbox':Cnumbox
+    //通过id拿到商品的选购数量
+    getTotal(id) {
+      // return goodsStorage.get(id);
+      return this.$store.state[id];
+    },
+    //更新指定商品的购买数量
+    upTotal(id, total) {
+      //要手动更新才能触发购买数量和总价格的修改
+      //  this.shopcartList[0].selected = !this.shopcartList[0].selected;
+      //  this.shopcartList[0].selected = !this.shopcartList[0].selected;
+      //  goodsStorage.set(id,total);
+      //   // 把商品的总数挂载上去
+      //  document.querySelector('.mui-badge').innerHTML = goodsStorage.get();
+      this.$store.commit('set', { id, total });
+    },
+    //付款
+    payment() {
+      //只要有一个商品被选中,并且不为0,那么结果就是true
+      let hasSelected = this.shopcartList.some(item => item.selected && goodsStorage.get(item.id));
+      if (!hasSelected) {
+        Toast('请至少选购一件商品');
+      }
+      //使用正则判断用户是否登陆,登陆成功后跳转到订单页
+      else if (!/SESSIONID=\w+/.test(document.cookie)) {
+        // Toast('跳转到登陆');
+        this.$router.push({ name: 'login', query: { nextpage: '/shopcart/order' } })
+      }
+      // 选购了商品，也登陆了，才能转到订单页
+      else {
+        //  Toast('跳转到订单页面');
+        this.$router.push('/shopcart/order');
+      }
     }
-    
+
+  }, computed: {
+    //总数量
+    sumTotal() {
+      // 遍历取出每个商品的数量，如果这个商品是未选中状态，那么数量设为0
+      //  let totalArr = this.shopcartList.map(item => item.selected?+goodsStorage.get(item.id):0);
+      let totalArr = this.shopcartList.map(item => item.selected ? + this.$store.state[item.id] : 0);
+      return totalArr.length && totalArr.reduce((v1, v2) => v1 + v2);
+    },
+    //总价格
+    priceTotal() {
+      // 遍历取出每个商品的数量，如果这个商品是未选中状态，那么数量设为0
+      //  let priceArr = this.shopcartList.map(item => item.selected?+goodsStorage.get(item.id)*item.sell_price:0);
+      let priceArr = this.shopcartList.map(item => item.selected ? + this.$store.state[item.id] * item.sell_price : 0);
+      return priceArr.length && priceArr.reduce((v1, v2) => v1 + v2);
+    }
+  },
+  created() {
+    this.getShopcartList();
+  },
+  components: {
+    "v-title": Ctitle,
+    'v-numbox': Cnumbox
+  }
+
 }
 </script>
 
 <style lang='less'>
- .shopcart-list {
-    .goods {
-      border-bottom: 1px solid rgba(0, 0, 0, 0.3);
-      height: 102px;
-      display: flex;
-      padding: 5px;
-      .switch {
-        flex: 0 0 52px;
-      }
-      .img {
-        margin-left: 5px;
-        height: 75px;
-        width: 75px;
-        flex: 0 0 85px;
-      }
-      .inforight {
-        margin-left: 5px;
-        flex: 1;
-      }
-      .inforight ul {
-        padding-left: 0px;
-      }
-      .inforight li {
-        list-style: none;
-        display: inline-block;
-      }
-      .inforight h4 {
-        color: #0094ff;
-        font-size: 14px;
-      }
-      .bottom li:first-child {
-        color: red;
-        margin-right: 5px;
-      }
-      .bottom li:last-child {
-        margin-left: 5px;
-      }
+.shopcart-list {
+  .goods {
+    border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+    height: 102px;
+    display: flex;
+    padding: 5px;
+    .switch {
+      flex: 0 0 52px;
     }
-    .total {
-      height: 84px;
-      background-color: rgba(0, 0, 0, 0.1);
-      display: flex;
-      padding: 5px 14px;
-      ul {
-		    padding-left: 0px;
-		    margin: 14px 0;
-		    li {
-		      list-style: none;
-		      font-size: 14px;
-		    }
-		  }
-		  &_val {
-		  	flex: 1;
-		  }
-		  &_btn {
-		  	flex: 0 0 60px;
-	      margin: 18px 0 0 0;
-		  }
+    .img {
+      margin-left: 5px;
+      height: 75px;
+      width: 75px;
+      flex: 0 0 85px;
+    }
+    .inforight {
+      margin-left: 5px;
+      flex: 1;
+    }
+    .inforight ul {
+      padding-left: 0px;
+    }
+    .inforight li {
+      list-style: none;
+      display: inline-block;
+    }
+    .inforight h4 {
+      color: #0094ff;
+      font-size: 14px;
+    }
+    .bottom li:first-child {
+      color: red;
+      margin-right: 5px;
+    }
+    .bottom li:last-child {
+      margin-left: 5px;
     }
   }
+  .total {
+    height: 84px;
+    background-color: rgba(0, 0, 0, 0.1);
+    display: flex;
+    padding: 5px 14px;
+    ul {
+      padding-left: 0px;
+      margin: 14px 0;
+      li {
+        list-style: none;
+        font-size: 14px;
+      }
+    }
+    &_val {
+      flex: 1;
+    }
+    &_btn {
+      flex: 0 0 60px;
+      margin: 18px 0 0 0;
+    }
+  }
+}
 </style>
